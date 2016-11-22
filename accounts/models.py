13411200f3b013daea04bd887c9dd682b22faf83
车亮亮 <liangliangyy@gmail.com>
2016-11-22 18:13:15 +0800

    Update models.py
    修改用户model

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,8 @@ from django.core.urlresolvers import reverse
 
 
 # Create your models here.
+
+"""
 class BlogUserManager(BaseUserManager):
     def create_user(self, email, username, password=None):
         user = self.model(
@@ -12,12 +14,12 @@ class BlogUserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-
+"""
 
 class BlogUser(AbstractUser):
     nickname = models.CharField('昵称', max_length=50, blank=True)
     mugshot = models.ImageField('头像', upload_to='upload/mugshots', blank=True)
-    objects = BlogUserManager()
+    #objects = BlogUserManager()
 
     def get_absolute_url(self):
         return reverse('blog:author_detail', kwargs={'author_name': self.username})
