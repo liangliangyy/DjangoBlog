1629d090886bb6149532622ed65ea601314fd9a8
liangliangyy <liangliangyy>
2017-01-16 14:45:03 +0800

    修复评论bug

--- comments/forms.py
+++ comments/forms.py
@@ -43,7 +43,7 @@ class LoginCommentForm(ModelForm):
 
 class CommentForm(ModelForm):
     url = forms.URLField(label='网址', required=False)
-    email = forms.EmailField(label='电子邮箱', required=False)
+    email = forms.EmailField(label='电子邮箱', required=True)
     name = forms.CharField(label='姓名', widget=forms.TextInput(attrs=
                                                               {'value': "", 'size': "30", 'maxlength': "245",
                                                                'aria-required': 'true'}
