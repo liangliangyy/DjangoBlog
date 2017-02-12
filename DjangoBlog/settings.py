c42f708cb5db7ac16982e974a90e1a9c12f8eb23
车亮亮 <liangliangyy@gmail.com>
2017-02-12 14:25:31 +0800

    添加js/css压缩

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'blog',
     'accounts',
     'comments',
-    'oauth'
+    'oauth',
+    'compressor'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -272,3 +273,26 @@ LOGGING = {
         },
     }
 }
+
+
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    #other
+    'compressor.finders.CompressorFinder',
+)
+COMPRESS_ENABLED = True
+#COMPRESS_OFFLINE = True
+
+"""
+COMPRESS_CSS_FILTERS = [
+    #creates absolute urls from relative ones
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    #css minimizer
+    'compressor.filters.cssmin.CSSMinFilter'
+]
+COMPRESS_JS_FILTERS = [
+    'compressor.filters.jsmin.JSMinFilter'
+]
+"""
