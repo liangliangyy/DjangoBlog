7e5c3eac1253612e79fa035bca207337a1649fe2
liangliang <liangliangyy@gmail.com>
2017-08-04 21:04:00 +0800

    修改邮件服务器至zoho

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -226,7 +226,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = True
 
-EMAIL_HOST = 'smtp.exmail.qq.com'
+EMAIL_HOST = 'smtp.zoho.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('DJANGO_EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_EMAIL_PASSWORD')
