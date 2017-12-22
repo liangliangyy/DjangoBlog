ebcdb08f809b98f523d51b454cc3241596774140
liangliang <liangliangyy@gmail.com>
2017-12-23 01:12:02 +0800

    管理后台功能修改 👮‍♂️

--- comments/admin.py
+++ comments/admin.py
@@ -4,11 +4,24 @@ from django.contrib import admin
 from .models import Comment
 
 
+def disable_commentstatus(modeladmin, request, queryset):
+    queryset.update(is_enable=False)
+
+
+def enable_commentstatus(modeladmin, request, queryset):
+    queryset.update(is_enable=True)
+
+
+disable_commentstatus.short_description = '禁用评论'
+enable_commentstatus.short_description = '启用评论'
+
+
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'body', 'author', 'article', 'last_mod_time')
+    list_display = ('id', 'body', 'author', 'is_enable', 'article', 'last_mod_time')
     list_display_links = ('id', 'body')
-    list_filter = ('author', 'article',)
+    list_filter = ('author', 'article', 'is_enable')
     exclude = ('created_time', 'last_mod_time')
+    actions = [disable_commentstatus, enable_commentstatus]
 
 
 admin.site.register(Comment, CommentAdmin)
