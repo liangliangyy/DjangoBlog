d3d3f8f8955d2e81f12a13425a61dc521509d527
root <root@instance-1.c.lylinux-186314.internal>
2018-04-08 19:44:29 +0800

    add docker_start

---
+++ bin/docker_start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+NAME="djangoblog" # Name of the application
+DJANGODIR=/code/DjangoBlog # Django project directory
+USER=root # the user to run as
+GROUP=root # the group to run as
+NUM_WORKERS=3 # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=DjangoBlog.settings # which settings file should Django use
+DJANGO_WSGI_MODULE=DjangoBlog.wsgi # WSGI module name
+
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+python manage.py makemigrations 
+python manage.py migrate
+python manage.py collectstatic --noinput 
+python manage.py compress --force
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec gunicorn ${DJANGO_WSGI_MODULE}:application \
+--name $NAME \
+--workers $NUM_WORKERS \
+--user=$USER --group=$GROUP \
+--bind 0.0.0.0:8000 \
+--log-level=debug \
+--log-file=-
+
