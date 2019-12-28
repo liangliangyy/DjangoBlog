08dc88ae5a6ce6f8533c113bc382523deb5e9899
Marat Tuktarov <mtuktarov@yahoo.com>
2019-12-28 11:54:13 +0300

    delete_baidu

--- blog/management/commands/ping_baidu.py
+++ blog/management/commands/ping_baidu.py
@@ -9,7 +9,6 @@ site = get_current_site().domain
 
 
 class Command(BaseCommand):
-    help = 'notify baidu url'
 
     def add_arguments(self, parser):
         parser.add_argument('data_type', type=str, choices=['all', 'article', 'tag', 'category'],
@@ -37,5 +36,4 @@ class Command(BaseCommand):
                 urls.append(self.get_full_url(url))
 
         self.stdout.write(self.style.SUCCESS('start notify %d urls' % len(urls)))
-        SpiderNotify.baidu_notify(urls)
         self.stdout.write(self.style.SUCCESS('finish notify'))
