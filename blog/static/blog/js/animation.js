0a3493770f8610e38884d8e9912a74e28d4fdc0a
Marat Tuktarov <mtuktarov@yahoo.com>
2020-01-12 01:41:04 +0300

    reverted changes

---
+++ blog/static/blog/js/animation.js
@@ -0,0 +1,13 @@
+function animateCSS(element, animationName, callback) {
+    const node = document.querySelector(element)
+    node.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+
+        if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
+}
