1366fb0a62ac190465aaf2ad4d518296e523c103
kentio <13550898+kentio@users.noreply.github.com>
2020-03-20 20:32:13 +0800

    refactor(midd): 优化blog app中间件逻辑

--- blog/middleware.py
+++ blog/middleware.py
@@ -25,21 +25,22 @@ class OnlineMiddleware(object):
         super().__init__()
 
     def __call__(self, request):
+        ''' page render time '''
         start_time = time.time()
         response = self.get_response(request)
-        http_user_agent = request.META.get('HTTP_USER_AGENT', [])
-        if 'Spider' in http_user_agent or 'spider' in http_user_agent:
-            return response
-        try:
-            cast_time = time.time() - start_time
-            if ELASTICSEARCH_ENABLED:
-                time_taken = round((cast_time) * 1000, 2)
-                url = request.path
-                from django.utils import timezone
+        http_user_agent = request.META.get('HTTP_USER_AGENT', '')
 
-                ElaspedTimeDocumentManager.create(url=url, time_taken=time_taken, log_datetime=timezone.now(),
-                                                  type='blog', useragent=http_user_agent)
-            response.content = response.content.replace(b'<!!LOAD_TIMES!!>', str.encode(str(cast_time)[:5]))
-        except:
-            pass
+        if 'spider'.upper() not in http_user_agent.upper():
+            try:
+                cast_time = time.time() - start_time
+                if ELASTICSEARCH_ENABLED:
+                    time_taken = round((cast_time) * 1000, 2)
+                    url = request.path
+                    from django.utils import timezone
+
+                    ElaspedTimeDocumentManager.create(url=url, time_taken=time_taken, log_datetime=timezone.now(),
+                                                      type='blog', useragent=http_user_agent)
+                response.content = response.content.replace(b'<!!LOAD_TIMES!!>', str.encode(str(cast_time)[:5]))
+            except Exception as e:
+                print("Error OnlineMiddleware: %s" % e)
         return response
