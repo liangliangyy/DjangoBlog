41234f7a07cfc36d55be0c6def49a51b5b31666e
liangliang <liangliangyy@gmail.com>
2021-07-13 21:47:51 +0800

    docker support

--- bin/docker_start.sh
+++ bin/docker_start.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
+
 NAME="djangoblog" # Name of the application
-DJANGODIR=/code/DjangoBlog # Django project directory
+DJANGODIR=/var/www/DjangoBlog # Django project directory
+SOCKFILE=/var/www/DjangoBlog/run/gunicorn.sock # we will communicte using this unix socket
 USER=root # the user to run as
 GROUP=root # the group to run as
-NUM_WORKERS=1 # how many worker processes should Gunicorn spawn
-#DJANGO_SETTINGS_MODULE=DjangoBlog.settings # which settings file should Django use
+NUM_WORKERS=3 # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=DjangoBlog.settings # which settings file should Django use
 DJANGO_WSGI_MODULE=DjangoBlog.wsgi # WSGI module name
-
 
 echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
 cd $DJANGODIR
-
+source /var/www/dev/python3/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
-#pip install -Ur requirements.txt -i http://pypi.douban.com/simple/  --trusted-host pypi.douban.com && \
-#        pip install gunicorn  -i http://pypi.douban.com/simple/ --trusted-host pypi.douban.com
-python manage.py makemigrations && \
-  python manage.py migrate && \
-  python manage.py collectstatic --noinput  && \
-  python manage.py compress --force && \
-  python manage.py build_index && \
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec gunicorn ${DJANGO_WSGI_MODULE}:application \
---name $NAME \
---workers $NUM_WORKERS \
---user=$USER --group=$GROUP \
---bind 0.0.0.0:8000 \
---log-level=debug \
---log-file=- \
---worker-class gevent \
---threads 4
+exec /var/www/dev/python3/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+	--name $NAME \
+	--workers $NUM_WORKERS \
+	--user=$USER --group=$GROUP \
+	--bind=unix:$SOCKFILE \
+	--log-level=debug \
+	--log-file=-
+
