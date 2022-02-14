f4d7f9e153585e25f15df8fec189544077f80180
liangliangyy <liangliangyy@gmail.com>
2022-02-14 14:51:38 +0800

    修复部分代码漏洞

--- accounts/views.py
+++ accounts/views.py
@@ -153,8 +153,8 @@ def account_result(request):
     if type and type in ['register', 'validation']:
         if type == 'register':
             content = '''
-    恭喜您注册成功，一封验证邮件已经发送到您 {email} 的邮箱，请验证您的邮箱后登录本站。
-    '''.format(email=user.email)
+    恭喜您注册成功，一封验证邮件已经发送到您的邮箱，请验证您的邮箱后登录本站。
+    '''
             title = '注册成功'
         else:
             c_sign = get_sha256(get_sha256(settings.SECRET_KEY + str(user.id)))
--- comments/urls.py
+++ comments/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 app_name = "comments"
 urlpatterns = [
-    # url(r'^po456stcomment/(?P<article_id>\d+)$', views.CommentPostView.as_view(), name='postcomment'),
     path(
         'article/<int:article_id>/postcomment',
         views.CommentPostView.as_view(),
