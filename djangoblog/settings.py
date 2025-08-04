5155d4dc476d46224abe67327914ef8519bae4d4
Youmu <yantd@qq.com>
2025-08-04 15:35:03 +0800

    some cleanup

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -36,9 +36,9 @@ DEBUG = env_to_bool('DJANGO_DEBUG', True)
 TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['*', '127.0.0.1', 'example.com']
+ALLOWED_HOSTS = ['*', '127.0.0.1', 'www.yantengda.com']
 # django 4.0新增配置
-CSRF_TRUSTED_ORIGINS = ['http://example.com']
+CSRF_TRUSTED_ORIGINS = ['https://*.yantengda.com', 'http://127.0.0.1']
 # Application definition
 
 
